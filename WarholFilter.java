a1ba2429ee993c2a9a726f30a4e098edebbaf26e
curban2336 <curban841@gmail.com>
2021-04-17 19:32:26 -0400

    Finished first warhol filter
    After much trial and error I figured out how to code the individual sections to hold the proper color tints

--- WarholFilter.java
+++ WarholFilter.java
@@ -26,11 +26,8 @@ public class WarholFilter extends Filter
     {
         int height = image.getHeight();
         int width = image.getWidth();
-        RedTint rFilter = new RedTint("Red Tint");
         OFImage rImage = image;
-        GreenTint gFilter = new GreenTint("Green Tint");
         OFImage gImage = image;
-        BlueTint bFilter = new BlueTint("Blue Tint");
         OFImage bImage = image;
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
@@ -42,22 +39,70 @@ public class WarholFilter extends Filter
             for(int x = 0; x < (width/2); x++) {
                 Color rPixel = rImage.getPixel(x, y);
                 rImage.setPixel(x+(width/2),y,rPixel);
+                int pixel = rImage.getRGB(x+(width/2),y);
+                Color color = new Color(pixel, true);
+                int redValue = color.getRed();
+                int blueValue = color.getBlue();
+                int greenValue = color.getGreen();
+                
+                if(redValue+40<=255){
+                    redValue += 40;
+                }
+                else
+                {
+                    blueValue -= 40;
+                    greenValue -= 40;
+                }
+                Color newColor = new Color(redValue, greenValue, blueValue);
+                
+                rImage.setRGB(x+(width/2),y,newColor.getRGB());
             }
         }
         for(int y = 0; y < (height/2); y++) {
             for(int x = 0; x < (width/2); x++) {
                 Color gPixel = gImage.getPixel(x, y);
                 gImage.setPixel(x,y+(height/2),gPixel);
+                int pixel = gImage.getRGB(x,y+(height/2));
+                Color color = new Color(pixel, true);
+                int redValue = color.getRed();
+                int blueValue = color.getBlue();
+                int greenValue = color.getGreen();
+                
+                if(greenValue+40<=255){
+                    greenValue += 40;
+                }
+                else
+                {
+                    blueValue -= 40;
+                    redValue -= 40;
+                }
+                Color newColor = new Color(redValue, greenValue, blueValue);
+                
+                rImage.setRGB(x,y+(height/2),newColor.getRGB());
             }
         }
         for(int y = 0; y < (height/2); y++) {
             for(int x = 0; x < (width/2); x++) {
                 Color bPixel = bImage.getPixel(x, y);
                 bImage.setPixel(x+(width/2),y+(height/2),bPixel);
+                int pixel = bImage.getRGB(x,y+(height/2));
+                Color color = new Color(pixel, true);
+                int redValue = color.getRed();
+                int blueValue = color.getBlue();
+                int greenValue = color.getGreen();
+                
+                if(blueValue+40<=255){
+                    blueValue += 40;
+                }
+                else
+                {
+                    greenValue -= 40;
+                    redValue -= 40;
+                }
+                Color newColor = new Color(redValue, greenValue, blueValue);
+                
+                rImage.setRGB(x+(width/2),y+(height/2),newColor.getRGB());
             }
         }
-        rFilter.apply(rImage);
-        gFilter.apply(gImage);
-        bFilter.apply(bImage);
     }
 }
